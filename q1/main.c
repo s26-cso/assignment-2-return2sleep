55003af504aa553387a1188ece9b7e0f02b3633a
return2sleep <darshanlavu09@gmail.com>
2026-04-14 23:48:22 +0530

    Create main.c

---
+++ q1/main.c
@@ -0,0 +1,4 @@
+extern struct Node* make_node(int val);
+extern struct Node* insert(struct Node* root, int val);
+extern struct Node* get(struct Node* root, int val);
+extern int getAtMost(int val, struct Node* root);
